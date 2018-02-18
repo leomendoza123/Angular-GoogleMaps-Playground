f744ac7098ade9578505b349151f6df2a63ecaa8
Leonardo Mendoza <leomendoza123@gmail.com>
2018-02-18 09:51:07 -0600

    Starts Adding Map Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
 import { environment } from '../environments/environment';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, ,
+    BrowserModule, 
     AgmCoreModule.forRoot({
       apiKey:  environment.googleMapKey
-    })
+    }),
+    AgmSnazzyInfoWindowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
